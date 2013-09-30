732cdbe8f0e59d8eff8e98700b5894340f498cf0
Vladislav Koldobskiy <admin@nevergone.ru>
2013-10-01 00:13:59 +0400

    Camera button support (1/2)
    Add new bitmask for Camera button and "hasCamera" boolean.
    For those devices that have dedicated Camera key
    (like Sony ZL/ZR/Z1/...)
    Change-Id: I1a6681664f9f3fd3f30221ba10b082cb5f8cafca
    
    Camera button support: make it actually work
    Previous commits were merged so fast that I didn't even
    added the code that actually DOES launch the camera.
    Let me fix that.
    Change-Id: I4fcb3c46e5fd50e273dcf19fd3eb35a3f9d979e0
    
    Camera button support: useful features & bugfixes (1/2)
    Screw the remapping, let's make something more useful!
    Replaced the code to actually launch Camera app, not just fire the shutter.
    I won't remove it: it's cool to have an option to double-click Home for Camera.
    Added:
      * Camera wake (with button half-press) and unlock options
      * "Peek": releasing the half-pressed Camera button will
        turn the screen back off
      * Camera playback control
    
    Change-Id: Id49e09e20d81cd80d9800ab8ab0f200d44485a09
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/config.xml
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3423,6 +3423,24 @@ public final class Settings {
         public static final String VOLBTN_MUSIC_CONTROLS = "volbtn_music_controls";
 
         /**
+         * Whether to wake the screen with the camera key half-press.
+         * @hide
+         */
+        public static final String CAMERA_WAKE_SCREEN = "camera_wake_screen";
+
+        /**
+         * Whether or not to send device back to sleep if Camera button is released ("Peek")
+         * @hide
+         */
+        public static final String CAMERA_SLEEP_ON_RELEASE = "camera_sleep_on_release";
+
+        /**
+         * Whether or not camera button music controls should be enabled to play/pause media tracks
+         * @hide
+         */
+        public static final String CAMERA_MUSIC_CONTROLS = "camera_music_controls";
+
+        /**
          * Whether or not to launch default music player when headset is connected
          * @hide
          */
@@ -3640,6 +3658,7 @@ public final class Settings {
           * 3 - Search
           * 4 - Voice search
           * 5 - In-app search
+	  * 6 - Launch Camera
           * @hide
           */
          public static final String KEY_HOME_LONG_PRESS_ACTION = "key_home_long_press_action";
@@ -4408,6 +4427,12 @@ public final class Settings {
          * @hide
          */
         public static final String HOME_UNLOCK_SCREEN = "home_unlock_screen";
+
+        /**
+         * Whether to unlock the screen with the camera key.  The value is boolean (1 or 0).
+         * @hide
+         */
+        public static final String CAMERA_UNLOCK_SCREEN = "camera_unlock_screen";
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
