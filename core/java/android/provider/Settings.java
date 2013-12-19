048e37c245a03b4fd381917b7d01b85fac6c3e31
scott <scott@ubuntu>
2013-12-19 00:21:01 -0500

    Revert "Framework: Bring back Volume dialog features (2/2)"
    This reverts commit d2184ff0bebfb9904f37db623ea9537303085aae.

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -2079,7 +2079,7 @@ public final class Settings {
          * Whether to prevent loud volume levels when headset is first plugged in.
          * @hide
          */
-        public static final String SAFE_HEADSET_VOLUME = "safe_headset_volume";
+        public static final String SAFE_HEADSET_VOLUME_RESTORE = "safe_headset_volume_restore";
 
         /**
          * Master volume (float in the range 0.0f to 1.0f).
